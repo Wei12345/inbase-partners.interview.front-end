6b7c073300d60265e9855ae6ba40f9c0c641d60f
Wei <weihsu@inquartik.com>
2021-10-20 20:50:26 +0800

    feat: app add symbol-selector

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { useState, useCallback, useEffect, useMemo } from 'react';
+import Autocomplete from "./components/Autocomplete";
+import { getExchangeInformation } from './apis/market-data';
+
 function App() {
-  return null
+  const [symbols, setSymbols] = useState([]);
+  const symbolAutocompleteOptions = useMemo(() => symbols.map(({ symbol }) => ({ label: symbol })), [symbols])
+
+  useEffect(() => {
+    const fetchExchangeInformation = async () => {
+      const exchangeInformation = await getExchangeInformation();
+      setSymbols(exchangeInformation.symbols);
+    }
+    fetchExchangeInformation()
+  }, [])
+
+  const [symbol, setSymbol] = useState('');
+  const handleSymbolChange = useCallback((value) => {
+    setSymbol(value)
+  }, []);
+
+  return (
+    <>
+      <Autocomplete
+        value={symbol}
+        onChange={handleSymbolChange}
+        options={symbolAutocompleteOptions}
+      />
+    </>
+  )
 }
 
 export default App;
